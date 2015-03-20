ac4bcc9b3a0533fbe9935464211e1d143c352d5f
mizuki.sonoko <mizuki.sonoko@gmail.com>
2015-03-21 03:42:20 +0900

    [fix] It applys case 404 Not found

--- test.sh
+++ test.sh
@@ -14,17 +14,26 @@ do
 	echo ${request_o}
 	curl $request_i -o i_${i}
 	curl $request_o -o o_${i}
+
+	cat i_${i} | grep "<html>"
+	if [ $? -eq 0 ] ; then
+		rm i_${i} o_${i} 
+		break
+	fi
+
 done
 
 make
 
-i=0
-while [ $i -ne 20 ]
+while [ $i -ne 1 ]
 do
-	i=`expr $i + 1`
+
+	i=`expr $i - 1`
 	./run < i_${i} > my_out_${i}
-	diff -s o_${i} my_out_${i}
+	diff -s o_${i} my_out_${i} | grep Files
+	diff -q o_${i} my_out_${i} | grep Files
 	rm i_${i} o_${i} my_out_${i} 
+
 done
 
 rm run
